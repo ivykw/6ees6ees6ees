7e801b1fa9e92fb9acbb5f13f70011c4fafe291d
ivykw <ivyw219@gmail.com>
2022-02-28 13:57:20 -0600

    All bees pass in ES6

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor() {
+    super();
+    console.log(this)
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage() {
+    var treasure;
+    this.treasureChest.push(treasure);
+  }
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor() {
+    super();
+    console.log(this)
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+  makeHoney() {
+    this.honeyPot += 1;
+  }
+  giveHoney() {
+    this.honeyPot -= 1;
+  }
 };
